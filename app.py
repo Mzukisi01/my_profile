b3e5480b63f2616ecddc94987f352ce7d29c9005
Mzukisi01 <mzukisisobhoyise@gmail.com>
2025-01-29 13:56:52 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+color = st.select_slider(
+    "Select a color of the rainbow",
+    options=[
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ],
+)
+st.write("My favorite color is", color)
